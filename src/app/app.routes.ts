6c682cf165f88c841ec7133db7ac059e4da5bb35
Alfredo Goeten <alfredo.goeten@outlook.com>
2025-07-21 02:07:45 -0300

    creates components and folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TasksPage } from './features/tasks/pages/tasks-page/tasks-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks',
+    component: TasksPage,
+    title: 'My Tasks',
+  },
+  {
+    path: '**',
+    redirectTo: 'tasks',
+  },
+];
